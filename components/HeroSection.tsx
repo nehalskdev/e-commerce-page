de77101af12ddc28e612b77114a8859ae01908d9
nehalskdev <nikesk1999@gmail.com>
2026-02-17 21:31:18 +0530

    updated text

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <section className="text-center py-20">
-      <h2 className="text-5xl font-extrabold mb-6">Welcome to Nehal Store</h2>
+      <h2 className="text-5xl font-extrabold mb-6">
+        Welcome to Your one-stop Store
+      </h2>
       <p className="text-gray-600 text-lg mb-8">
         Modern products for modern lifestyle.
       </p>
